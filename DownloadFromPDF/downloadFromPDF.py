09c6c472b42bc123518e35b29150ebab2af9ba6e
PabloAceG <pabloacereda98@gmail.com>
2020-05-26 11:40:16 +0200

    Initial DownloadFromPDF commit: (more) - Code header creation. - Empty README, ready to be filled in. - Sample PDF with links to download. - Modules requirements (pdfminer).

---
+++ DownloadFromPDF/downloadFromPDF.py
@@ -0,0 +1,37 @@
+""" This module implements an image convertor to PDF files.
+
+After converting the images to PDF, all the images in the same folder are 
+compressed to a ZIP file.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Pablo Acereda García"
+__authors__ = ["Pablo Acereda García"]
+__contact__ = "p.aceredag@gmail.com"
+__copyright__ = "Copyright 2020"
+__credits__ = ["Pablo Acereda García"]
+__date__ = "2020/04/09"
+__deprecated__ = False
+__email__ =  "p.aceredag@gmail.com"
+__license__ = "GPLv3"
+__maintainer__ = "Pablo Acereda García"
+__status__ = "Development"
+__version__ = "0.0.1"
+
+import PDFMiner
+
+''' 
+Main class execution
+'''
+if __name__ == "__main__":
+
+    print('Starting download!')
